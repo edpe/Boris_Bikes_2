f004ccf5c446949828a3e0fac1a47b0d0ec02a18
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-01-04 16:01:14 +0000

    updated nothing

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,14 +1,21 @@
 class DockingStation
+  attr_reader :bikes
+
+  def initialize
+    @bikes = []
+  end
 
   def release_bike
     Bike.new
   end
 
   def dock(bike)
-    
+    @bikes << bike
   end
 
 end
+
+
 
 class Bike
 
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -12,4 +12,15 @@ describe DockingStation do
     expect(subject).to respond_to(:dock).with(1).argument
   end
 
+  it "chech if dock is empty" do
+    expect(subject.bikes.empty?).to eq true
+    #expect(:@bikes.empty?).to eq false
+  end
+
+  #it "does the bike dock successfully" do
+    #test_bike = subject.release_bike
+    #expect(subject.dock(test_bike).include?(test_bike)).to eq false
+
+  #end
+
 end
