7f789cfc39eae66e4124dd5f600700f63cdf2a27
Ed Perkins <ed@edperkins.co.uk>
2018-01-05 15:55:50 +0000

    add constant DEFAULT_CAPACITY

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,5 +1,6 @@
 class DockingStation
   attr_reader :bikes
+  DEFAULT_CAPACITY = 20
 
   def initialize
     @bikes = []
@@ -25,7 +26,7 @@ class DockingStation
   end
 
   def full?
-    @bikes.count >= 20
+    @bikes.count >= DEFAULT_CAPACITY
   end
 
 end
